5de9b3d7e1cea3176690d36d444cb9c0542890e1
Reza Moghaddam <info@moghaddam24.ir>
2020-06-27 15:31:07 +0430

    Update trello.user.js

--- trello.user.js
+++ trello.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trello jdate
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://trello.com/*
@@ -84,8 +84,75 @@ function updateIt(){
         $(".days-of-week .quiet:nth(6)").html("شنبه");
         $(".days-of-week").attr("data-r324", "OK");
     }
+    var members = $(".b-avatar__image");
+    for(var i=0; i<members.length; i++){
+        var a = $(members[i]).attr('style');
+        var id = a.slice(a.indexOf(".com/") + 5, -42);
+        if( id.indexOf("/") == -1 ){
+            var src = $(".member-avatar[src*="+id+"]")[0].src;
+        $(members[i]).attr('style', 'background: url("'+src+'") center center / cover no-repeat;');
+        }
+    }
 }
+
+var holidaysShamsi = ['1399/05/18', '1399/06/08', '1399/06/09', '1399/07/17', '1399/07/26', '1399/08/04', '1399/08/13', '1399/10/28', '1399/11/22', '1399/12/07', '1399/12/21', '1399/12/30'];
+
+function ganttFarsi(){
+    $(".b-cell_holiday").removeClass("b-cell_holiday");
+    $(".b-cell_last").removeClass("b-cell_last");
+    $(".b-cell_right-border").removeClass("b-cell_right-border");
+
+    var ganttDay = $(".b-cell_today");
+    var gday = new Date();
+    for(var i=0; i<40; i++){
+        var TS = gday.getTime() + (86400000*i);
+        var curDate = new Date(TS);
+        if( i != 0 ){
+            ganttDay = ganttDay.next();
+        }
+        var dayHead = ganttDay[0];
+        var dayCell = ganttDay[1];
+        if( curDate.getDay() == 5 ){
+            $($(dayHead)[0]).addClass("b-cell_holiday b-cell_right-border");
+            $($(dayCell)[0]).addClass("b-cell_holiday b-cell_right-border");
+        }
+        if( holidaysShamsi.indexOf( new JDate( TS ).echo() ) != -1 ){
+            $($(dayHead)[0]).addClass("b-cell_holiday");
+            $($(dayCell)[0]).addClass("b-cell_holiday");
+        }
+        $(dayHead).find(".b-day__text").text( new JDate( TS ).echo("d") );
+    }
+}
+function ganttFarsiPre(){
+    var ganttDay = $(".b-cell_today");
+    var gday = new Date();
+    for(var i=0; i<40; i++){
+        var TS = gday.getTime() - (86400000*i);
+        var curDate = new Date(TS);
+        if( i != 0 ){
+            ganttDay = ganttDay.prev();
+        }
+        var dayHead = ganttDay[0];
+        var dayCell = ganttDay[1];
+        if( curDate.getDay() == 5 ){
+            $($(dayHead)[0]).addClass("b-cell_holiday b-cell_right-border");
+            $($(dayCell)[0]).addClass("b-cell_holiday b-cell_right-border");
+        }
+        if( holidaysShamsi.indexOf( new JDate( TS ).echo() ) != -1 ){
+            $($(dayHead)[0]).addClass("b-cell_holiday");
+            $($(dayCell)[0]).addClass("b-cell_holiday");
+        }
+        $(dayHead).find(".b-day__text").text( new JDate( TS ).echo("d") );
+    }
+}
+
 (function() {
     'use strict';
     setInterval(updateIt, 1000);
+
+    $(document).on("click", ".b-switch__last", function(){
+        $("#beacon-container").remove();
+        ganttFarsi();
+        ganttFarsiPre();
+    });
 })();
